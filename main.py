fa8fb85dfd12e682763f14c17ce9833b85651e24
andrew <aadi5934@uni.sydney.edu.au>
2024-01-30 18:31:30 +1100

    added requirements

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///mysite/finance.db")
+db = SQL("sqlite:///finance.db")
 
 @app.after_request
 def after_request(response):
